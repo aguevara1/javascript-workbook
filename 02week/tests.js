cc498d19c38469cd69088ee5730e97526fd73e04
aguevara1 <albguevara12@gmail.com>
2018-01-28 22:06:38 -0600

    worked updates on tests file

--- 02week/tests.js
+++ 02week/tests.js
@@ -14,49 +14,49 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 //write function isValid
-
-
+let newHand1='';
+let newHand2='';
 const isValid = (handOne, handTwo) => {
-  const newHand1 = handOne.toLowerCase().trim();
-  const newHand2 = handTwo.toLowerCase().trim();
+  newHand1 = handOne.toLowerCase().trim();
+  newHand2 = handTwo.toLowerCase().trim();
   const possibleHands = ["rock", "paper", "scissors"];
   if (possibleHands.indexOf(newHand1) !== -1 && possibleHands.indexOf(newHand2) !== -1) {
     return true;
   } else {
-    return false;
+    return "Invalid input! Enter(rock paper scissors)";
   }
 }
 
 const rockPaperScissors = (hand1, hand2) => {
 
-
   if (isValid(hand1, hand2) == true) {
 
-    if (hand1 === hand2) { //check if hand 1 is equal to hand 2
+    if (newHand1 === newHand2) { //check if hand 1 is equal to hand 2
       return "It's a tie!"; // if yes it's a tie
-    } else if (hand1 === 'rock') { //check if hand 1 is equal rock
-      if (hand2 === 'paper') { // check if hand 2 is equal to paper
+    } else if (newHand1 === 'rock') { //check if hand 1 is equal rock
+      if (newHand2 === 'paper') { // check if hand 2 is equal to paper
         return "Hand two wins!"; // if yes hand 2 wins
       } else {
+        //  console.log(hand1);
         return "Hand one wins!"; //one wins
       }
 
-    } else if (hand1 === 'paper') { // check if hand 1 is equal to paper
-      if (hand2 === 'rock') { //check if hand 2 is equal
+    } else if (newHand1 === 'paper') { // check if hand 1 is equal to paper
+      if (newHand2 === 'rock') { //check if hand 2 is equal
         return "Hand one wins!"; //if yes
       } else {
         return "Hand two wins!";
       }
     }
     // hand1 must be Scissors
-    if (hand2 === 'rock') {
+    if (newHand2 === 'rock') {
       return "Hand two wins!";
     } else {
       return "Hand one wins!";
     }
 
   } else {
-    return false;
+    return "Invalid input! Enter(rock paper scissors)";
   }
 
 
@@ -90,9 +90,9 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
     });
     it('should only accept rock paper scissors', () => {
-      assert.equal(isValid('hello', 'goodbye'), "Invalid input!");
-      assert.equal(isValid('hello', 'SCISSORS'), "Invalid input!");
-      assert.equal(isValid('rock ', 'hello'), "Invalid input!");
+      assert.equal(isValid('hello', 'goodbye'), "Invalid input! Enter(rock paper scissors)");
+      assert.equal(isValid('hello', 'SCISSORS'), "Invalid input! Enter(rock paper scissors)");
+      assert.equal(isValid('rock ', 'hello'), "Invalid input! Enter(rock paper scissors)");
     });
 
   });
