fafe9666c8ef362449135f1c693758caf86a239a
aguevara1 <albguevara12@gmail.com>
2018-07-31 17:15:26 -0500

    kept editing

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -23,27 +23,53 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-function horizontalWin() {
+function horizontalWin(playerTurn2) {
   // Your code here
+  if( board[0][0]===playerTurn2 && board[0][1]===playerTurn2 && board[0][2]===playerTurn2){
+       return true;
+  }else if(board[1][0]===playerTurn2 && board[1][1]===playerTurn2 && board[1][2]===playerTurn2){
+       return true;
+   }else if(board[2][0]===playerTurn2 && board[2][1]===playerTurn2 && board[2][2]===playerTurn2){
+        return true;
+    }
 }
 
-function verticalWin() {
+function verticalWin(playerTurn2) {
   // Your code here
+  if( board[0][0]===playerTurn2 && board[1][0]===playerTurn2 && board[2][0]===playerTurn2){
+       return true;
+  }else if(board[0][1]===playerTurn2 && board[1][1]===playerTurn2 && board[2][1]===playerTurn2){
+       return true;
+   }else if(board[0][2]===playerTurn2 && board[1][2]===playerTurn2 && board[2][2]===playerTurn2){
+        return true;
+    }
 }
 
-function diagonalWin() {
+function diagonalWin(playerTurn2) {
+  // Your code her
+  if( board[0][0]===playerTurn2 && board[1][1]===playerTurn2 && board[2][2]===playerTurn2){
+       return true;
+  }else if(board[0][2]===playerTurn2 && board[1][1]===playerTurn2 && board[2][0]===playerTurn2){
+       return true;
+    }
+
+    }
+
+function checkForWin(playerTurn1) {
   // Your code here
+  if(verticalWin(playerTurn1)){
+    return true;
+  }else if(horizontalWin(playerTurn1)){
+    return true;
+  }else if(diagonalWin(playerTurn1)){
+    return true;
+  }
 }
 
-function checkForWin() {
-  // Your code here
-}
-
-// check if input is vali
- function isValid(row1,column1){
-    const possibleNumbers=[0,1,2];
-   return possibleNumbers.indexOf(parseInt(row1,10)) !==-1 && possibleNumbers.indexOf(parseInt(column1,10))!== -1;
-
+// check if input is valid
+function isValid(row1,column1){
+  const possibleNumbers=[0,1,2];
+  return possibleNumbers.indexOf(parseInt(row1,10)) !==-1 && possibleNumbers.indexOf(parseInt(column1,10))!== -1;
 
 }
 
@@ -53,12 +79,21 @@ function ticTacToe(row, column) {
   // check for valid input
 
   if(isValid(row,column)){
-  board[row][column]='X';
+    board[row][column]=playerTurn;
+
+    if(checkForWin(playerTurn)){
+
+      console.log( playerTurn+' Won!!!!');
+       return true;
+    }else{
+      playerTurn= playerTurn==='X' ? playerTurn='O' : playerTurn='X';
+    }
 
 
   } else{
     console.log("Enter numbers 0-2 only!!!!!!!!");
   }
+
 
 
 }
@@ -75,8 +110,6 @@ function getPrompt() {
 
 }
 
-
-
 // Tests
 
 if (typeof describe === 'function') {
@@ -92,7 +125,7 @@ if (typeof describe === 'function') {
     });
     it('should check for vertical wins', () => {
       board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), true);
+      assert.equal(verticalWin(), 'true');
     });
     it('should check for horizontal wins', () => {
       board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
