8d220dbcd13db78dae9fee872d20d91b3f8720de
aguevara1 <albguevara12@gmail.com>
2018-08-01 02:03:27 -0500

    more and more edits

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -23,74 +23,100 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-function horizontalWin(playerTurn2) {
+function horizontalWin() {
   // Your code here
-  if( board[0][0]===playerTurn2 && board[0][1]===playerTurn2 && board[0][2]===playerTurn2){
-       return true;
-  }else if(board[1][0]===playerTurn2 && board[1][1]===playerTurn2 && board[1][2]===playerTurn2){
-       return true;
-   }else if(board[2][0]===playerTurn2 && board[2][1]===playerTurn2 && board[2][2]===playerTurn2){
-        return true;
-    }
+return board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn ||
+      board[1][0] == playerTurn && board[1][1] == playerTurn && board[1][2] == playerTurn ||
+      board[2][0] == playerTurn && board[2][1] == playerTurn && board[2][2] == playerTurn;
+
+
+/*  if (board[0][0] === turn && board[0][1] === turn && board[0][2] === turn) {
+    return true;
+  } else if (board[1][0] === turn && board[1][1] === turn && board[1][2] === turn) {
+    return true;
+  } else if (board[2][0] === turn && board[2][1] === turn && board[2][2] === turn) {
+    return true;
+  }  */
 }
 
-function verticalWin(playerTurn2) {
+function verticalWin() {
   // Your code here
-  if( board[0][0]===playerTurn2 && board[1][0]===playerTurn2 && board[2][0]===playerTurn2){
-       return true;
-  }else if(board[0][1]===playerTurn2 && board[1][1]===playerTurn2 && board[2][1]===playerTurn2){
-       return true;
-   }else if(board[0][2]===playerTurn2 && board[1][2]===playerTurn2 && board[2][2]===playerTurn2){
-        return true;
-    }
+    const verticalResult=board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn ||
+    board[0][1] == playerTurn && board[1][1] == playerTurn && board[2][1] == playerTurn ||
+    board[0][2] == playerTurn && board[1][2] == playerTurn && board[2][2] == playerTurn;
+
+    console.log('Inside VerticalWin function');
+    console.log(verticalResult);
+    return verticalResult;
+  // if (board[0][0] === turn && board[1][0] === turn && board[2][0] === turn) {
+  //   return true;
+  // } else if (board[0][1] === turn && board[1][1] === turn && board[2][1] === turn) {
+  //   return true;
+  // } else if (board[0][2] === turn && board[1][2] === turn && board[2][2] === turn) {
+  //   return true;
+  // }
 }
 
-function diagonalWin(playerTurn2) {
+function diagonalWin() {
   // Your code her
-  if( board[0][0]===playerTurn2 && board[1][1]===playerTurn2 && board[2][2]===playerTurn2){
-       return true;
-  }else if(board[0][2]===playerTurn2 && board[1][1]===playerTurn2 && board[2][0]===playerTurn2){
-       return true;
-    }
-
-    }
-
-function checkForWin(playerTurn1) {
-  // Your code here
-  if(verticalWin(playerTurn1)){
+   if( board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2] == playerTurn ||
+   board[0][2] == playerTurn && board[1][1] == playerTurn && board[2][0] == playerTurn){
+     return true;
+   }else{
+     return false;
+   }
+  /*
+  if (board[0][0] === turn && board[1][1] === turn && board[2][2] === turn) {
     return true;
-  }else if(horizontalWin(playerTurn1)){
-    return true;
-  }else if(diagonalWin(playerTurn1)){
+  } else if (board[0][2] === turn && board[1][1] === turn && board[2][0] === turn) {
     return true;
   }
+*/
+}
+
+function checkForWin() {
+  // Your code here
+  return verticalWin() || horizontalWin() || diagonalWin();
+/*
+   if (verticalWin(turn)) {
+      return true;
+    } else if (horizontalWin(turn)) {
+        return true;
+   } else if (diagonalWin(turn)) {
+       return true;
+     }
+*/
 }
 
 // check if input is valid
-function isValid(row1,column1){
-  const possibleNumbers=[0,1,2];
-  return possibleNumbers.indexOf(parseInt(row1,10)) !==-1 && possibleNumbers.indexOf(parseInt(column1,10))!== -1;
+function isValid(row1, column1) {
+  const possibleNumbers = [0, 1, 2];
+  return possibleNumbers.indexOf(parseInt(row1, 10)) !== -1 && possibleNumbers.indexOf(parseInt(column1, 10)) !== -1;
 
 }
 
+function reset(){
 
+
+
+}
 
 function ticTacToe(row, column) {
   // check for valid input
 
-  if(isValid(row,column)){
-    board[row][column]=playerTurn;
+  if (isValid(row, column)) {
+    board[row][column] = playerTurn;
 
-    if(checkForWin(playerTurn)){
+    if (checkForWin()) {
 
-      console.log( playerTurn+' Won!!!!');
-       return true;
-    }else{
-      playerTurn= playerTurn==='X' ? playerTurn='O' : playerTurn='X';
+      console.log(' Won!!!!');
+      return true;
+    } else {
+      playerTurn === 'X' ? playerTurn = 'O' : playerTurn = 'X';
     }
 
 
-  } else{
+  } else {
     console.log("Enter numbers 0-2 only!!!!!!!!");
   }
 
@@ -117,22 +143,42 @@ if (typeof describe === 'function') {
   describe('#ticTacToe()', () => {
     it('should place mark on the board', () => {
       ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepEqual(board, [
+        [' ', ' ', ' '],
+        [' ', 'X', ' '],
+        [' ', ' ', ' ']
+      ]);
     });
     it('should alternate between players', () => {
       ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepEqual(board, [
+        ['O', ' ', ' '],
+        [' ', 'X', ' '],
+        [' ', ' ', ' ']
+      ]);
     });
     it('should check for vertical wins', () => {
-      board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
-      assert.equal(verticalWin(), 'true');
+      board = [
+        [' ', 'X', ' '],
+        [' ', 'X', ' '],
+        [' ', 'X', ' ']
+      ];
+      assert.equal(verticalWin(), true);
     });
     it('should check for horizontal wins', () => {
-      board = [ ['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' '] ];
+      board = [
+        ['X', 'X', 'X'],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+      ];
       assert.equal(horizontalWin(), true);
     });
     it('should check for diagonal wins', () => {
-      board = [ ['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X'] ];
+      board = [
+        ['X', ' ', ' '],
+        [' ', 'X', ' '],
+        [' ', ' ', 'X']
+      ];
       assert.equal(diagonalWin(), true);
     });
     it('should detect a win', () => {
