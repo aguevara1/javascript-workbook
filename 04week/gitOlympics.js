31dcc8b12d8776ae8a0f85312ec51c3914a47354
aguevara1 <albguevara12@gmail.com>
2018-08-02 19:16:34 -0500

    first commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict';
+const printListOfOlympians=  (arr) => {
+  arr.forEach ( (item)=> {
+    console.log(item);
+  });
+}
+
+
+printListOfOlympians(['Levi','Jose','John','Kevin','Joseph']);
