5bdeb122124e599c5ebbb4f9f6e624e8e87efe47
aguevara1 <albguevara12@gmail.com>
2018-08-18 20:14:37 -0500

    more changes to file

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -7,13 +7,25 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+class Checkers {
+  constructor(letter) {
+    this.letter= letter;
+  }
 
-function Checker() {
-  // Your code here
 }
+
+//const symbol= new Checkers("A");
+
+/*
+function Checker() {
+    // Your code here
+
+}
+*/
 
 function Board() {
   this.grid = [];
+
   // creates an 8x8 array, filled with null values
   this.createGrid = function() {
     // loop to create the 8 rows
@@ -21,13 +33,23 @@ function Board() {
       this.grid[row] = [];
       // push in 8 columns of nulls
       for (let column = 0; column < 8; column++) {
-        this.grid[row].push(null);
+        if ((row + column) % 2 === 0) {
+          this.grid[row].push(null);
+        } else {
+          this.grid[row].push('valid');
+        }
+        //    this.grid[row].push('X');
+        //console.log(this.grid[row]);
       }
     }
   };
 
+
   // prints out the board
   this.viewGrid = function() {
+  //  console.log(this);
+  this.symbol='A';
+  //  console.log(this.symbol);
     // add our column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
     for (let row = 0; row < 8; row++) {
@@ -37,11 +59,14 @@ function Board() {
       for (let column = 0; column < 8; column++) {
         // if the location is "truthy" (contains a checker piece, in this case)
         if (this.grid[row][column]) {
+          console.log('in with symbol');
           // push the symbol of the check in that location into the array
           rowOfCheckers.push(this.grid[row][column].symbol);
+           console.log(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
           rowOfCheckers.push(' ');
+          console.log(this.grid[row][column]);
         }
       }
       // join the rowOfCheckers array to a string, separated by a space
@@ -54,8 +79,10 @@ function Board() {
 
   // Your code here
 }
-function Game() {
 
+
+
+function Game() {
   this.board = new Board();
 
   this.start = function() {
@@ -63,6 +90,11 @@ function Game() {
     // Your code here
   };
 }
+
+function moveChecker(piece1, piece2) {
+  console.log("hello i'm in moveChecker function");
+}
+
 
 function getPrompt() {
   game.board.viewGrid();
@@ -74,12 +106,13 @@ function getPrompt() {
   });
 }
 
+
 const game = new Game();
 game.start();
+//game.moveChecker();
 
 
 // Tests
-
 if (typeof describe === 'function') {
   describe('Game', () => {
     it('should have a board', () => {
@@ -90,8 +123,8 @@ if (typeof describe === 'function') {
     });
   });
 
-  describe('Game.moveChecker()', function () {
-    it('should move a checker', function () {
+  describe('Game.moveChecker()', function() {
+    it('should move a checker', function() {
       assert(!game.board.grid[4][1]);
       game.moveChecker('50', '41');
       assert(game.board.grid[4][1]);
